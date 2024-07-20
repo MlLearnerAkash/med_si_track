aa38a79aad7fbc90543e12bcbb5126367757002b
root <root@slim-snow-glows-fin-01.datacrunch.io>
2024-07-20 17:11:09 +0000

    train.py for resuming

--- train.py
+++ train.py
@@ -4,17 +4,17 @@ from wandb.integration.ultralytics import add_wandb_callback
 from ultralytics import YOLO
 
 # Initialize a Weights & Biases run
-wandb.init(project="opervu-120724", job_type="training")
+wandb.init(project="opervu-190724", job_type="training")
 
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8l.pt")  # load a pretrained model (recommended for training)
+model = YOLO("/root/med_si_track/topervu-190724/train/weights/last.pt")  # load a pretrained model (recommended for training)
 add_wandb_callback(model, enable_model_checkpointing=True)
 
 # Train the model with 2 GPUs
-results = model.train(project= "topervu-120724",data="coco8.yaml", epochs=1000, imgsz=2480, device=0, batch=3,
-                      plots =True, )
+results = model.train(project= "topervu-190724",data="coco8.yaml", epochs=1000, imgsz=2480, device=0, batch=3,
+                      plots =True, resume = True)
 
 # Finalize the W&B Run
 wandb.finish()
