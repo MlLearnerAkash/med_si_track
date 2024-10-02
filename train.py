fcbe1c450b41361a1aadec50952ead15a46141fb
root <root@akash.datacrunch.io>
2024-10-02 02:58:12 +0000

    Modified: config file

--- train.py
+++ train.py
@@ -4,17 +4,19 @@ from wandb.integration.ultralytics import add_wandb_callback
 from ultralytics import YOLO
 from ultralytics import RTDETR
 # Initialize a Weights & Biases run
-wandb.init(project="opervu-310824-sponge-needle-training", job_type="training")
+wandb.init(project="opervu-011024-needle-only-training", job_type="training")
 
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("/root/ws/med_si_track/opervu-310824-needle-sponge-training/train/weights/last.pt")  # load a pretrained model (recommended for training)
+model = YOLO("/mnt/data/weights/base_weight/weights/best.pt")  # load a pretrained model (recommended for training)
 add_wandb_callback(model, enable_model_checkpointing=False)
 
 # Train the model with 2 GPUsd
-results = model.train(project= "opervu-310824-needle-sponge-training",data="/mnt/data/sponge_needle/YOLODataset_seg/dataset.yaml", epochs=1000, imgsz=2480, device=0, batch=4,
-                      plots =True, resume = True, save_period=100)
+results = model.train(project= "/mnt/data/weights/opervu-011024-needle-only-training",
+                      data="/mnt/data/needle_images_only/needle_images/YOLODataset_seg/dataset.yaml", 
+                      epochs=1000, imgsz=2480, device=0, batch=4,
+                      plots =True, resume = False, save_period=100)
 
 # Finalize the W&B Run
 wandb.finish()
